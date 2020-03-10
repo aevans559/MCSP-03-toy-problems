5d301fc536c7565e588c68ca22ee9adc836e378b
Aaron Evans <aaronmevans14@gmail.com>
2020-03-10 10:01:07 -0500

    toy problem update

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,4 +6,49 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+var longestPalindrome = function(string) {
+    // reverse the string
+    // compare the reversed string to the original string
+    // find the longest instance where the elements inside of the indexes match
+
+    let copyArray = string.split('');
+    let reversed = string.split('').reverse();
+    let copyCurrent;
+    let reversedCurrent;
+    let front;
+    let back;
+
+    debugger;
+    if (JSON.stringify(copyArray) !== JSON.stringify(reversed)) {
+        for (let i = 0; i < copyArray.length; i++) {
+            copyCurrent = copyArray[i]
+            front = copyArray[0]
+        }
+        
+        for (let j = reversed.length; j > 0; j--) {
+            reversedCurrent = reversed[j]
+            back = reversed[0]
+        }
+    
+        if (front !== back) {
+            copyArray.shift();
+            reversed.shift()
+            // longestPalindrome(JSON.stringify(copyArray))
+        } else if (copyCurrent !== reversedCurrent) {
+            return 'no palindrome'
+        } else {
+            return JSON.stringify(copyArray)
+        }
+    } else {
+        return JSON.stringify(copyArray)
+    }
+};
+
+// console.log(longestPalindrome('My dad is a racecar athlete'));
+
+// do {
+//     if (copyArray[0] !== reversed[0]) {
+//         copyArray.shift();
+//         reversed.shift();
+//     }
+// } while (JSON.stringify(copyArray) !== JSON.stringify(reversed))
