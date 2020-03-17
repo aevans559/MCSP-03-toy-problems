20dd514b4b1a916c9db7de975e9da8110c558721
Aaron Evans <aaronmevans14@gmail.com>
2020-03-17 10:10:27 -0500

    roman numeral translator

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -28,4 +28,44 @@ var DIGIT_VALUES = {
 
 var translateRomanNumeral = function(romanNumeral) {
   // TODO: Implement me!
+  if (typeof romanNumeral !== 'string') {
+    return null;
+  }
+
+  if (romanNumeral.length === 1) {
+    return DIGIT_VALUES[romanNumeral]
+  }
+
+  let result = 0;
+  // put the characters of the string into an array
+  let romanArray = romanNumeral.split('');
+  let base = DIGIT_VALUES[romanArray[0]];
+  let left, right;
+  // iterate through the array and find the highest value
+  for (let i = 0; i < romanArray.length; i++) {
+    // save the highest value as a base
+      if (DIGIT_VALUES[romanArray[1]] > base) {
+        base = DIGIT_VALUES[romanArray[1]];
+        left = DIGIT_VALUES[romanArray[0]];
+      } else {
+        right = DIGIT_VALUES[romanArray[1]]    
+      }
+    }
+
+  result += base;
+
+  // check for elements to left of base value and subtract those elements
+  if (left < base) {
+    result -= left;
+    // check for elements to right of base value and add those elements
+  } else if (right > base) {
+    result += right;
+  } else if (base === base) {
+    result += right
+  } else {
+    return result;
+  }
+
+  // return final result value
+  return result;
 };
